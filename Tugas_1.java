bd743afc7e48122e5e61480f2a40b49f3df2e9d7
pnjia <panjiangkasaputra67@gmail.com>
2025-03-05 14:56:25 +0700

    first commit

---
+++ Tugas_1.java
@@ -0,0 +1,10 @@
+package Tugas_1;
+
+public class Tugas_1 {
+    public static void main(String[] args) {
+        System.out.println("Belajar JAVA");
+        System.out.println("Sangat mudah sekali");
+        System.out.println("dan sangat menyenangkan");
+
+    }
+}
